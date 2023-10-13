b7497394a0d3b087e14f41f83cac87fb49e6f404
vikasgadge28 <vickygadgeofficial@email.com>
2023-10-13 10:22:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+Shery.imageEffect("#back", {style: 5, config :{"a":{"value":1.49,"range":[0,30]},"b":{"value":-0.88,
+"range":[-1,1]},"aspect":{"value":1.8853305857426297},"gooey":{"value":true}
+,"infiniteGooey":{"value":true},"durationOut":{"value":1,"range":[0.1,5]},
+"durationIn":{"value":1.02,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},
+"maskVal":{"value":1.33,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],
+"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":1.5,"range":[0,10]},
+"metaball":{"value":0.21,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},
+"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.47,"range":[0,2]},
+"noise_scale":{"value":12.15,"range":[0,100]}},
+gooey: true})
+
+var elems = document.querySelectorAll(".elem");
+
+elems.forEach(function(elem){
+    var h1s = elem.querySelectorAll("h1");
+    var index =0;
+
+    var animating =false;
+    document.querySelector("#main")
+    .addEventListener("click", function(){
+         if(!animating){
+            gsap.to(h1s[index], {
+                top: '-=100%',
+                ease: Expo.easeInput,
+                duartion: 1,
+                onComplete: function(){
+                    gsap.set(this._targets[0], {top : "100%"});
+                    animating: false;
+                }
+              });
+             index === h1s.length-1 ? index =0: index++;
+        
+              gsap.to(h1s[index], {
+                top: '-=100%',
+                ease: Expo.easeInput,
+                duartion: 1,
+              });
+         }
+    });
+})
+
+
